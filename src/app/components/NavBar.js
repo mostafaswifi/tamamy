726c4ebe8a9ee3ce001378423c9a88a44d4a7c5f
Your GitHub Username <mostafakhederswifi@gmail.com>
2025-04-25 20:36:56 +0300

    m3

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -31,25 +31,15 @@ const NavBar = () => {
         <div className="offcanvas-body">
           <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li className="nav-item">
-              {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
               <Link className="nav-link active" aria-current="page" href="/observermap">observer</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <Link className="nav-link" href="/signature">signature</Link>
             </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
+            <li className="nav-item">
+              <Link className="nav-link" href="/">الرئيسية</Link>
             </li>
+           
           </ul>
           <form className="d-flex mt-3" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />

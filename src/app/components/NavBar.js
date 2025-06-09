f4b289a480946b15e8c97a03c545df54830c6345
Your GitHub Username <mostafakhederswifi@gmail.com>
2025-06-09 20:59:52 +0300

    prod2

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import Link from "next/link";
+import { Inter } from 'next/font/google';
 import { cairo } from "@next/font/google";
 import { dynamic } from "next/dynamic";
 

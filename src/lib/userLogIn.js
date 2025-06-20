d2ac14847fec29edb2cf3bac32352c47700ab834
Your GitHub Username <mostafakhederswifi@gmail.com>
2025-06-20 23:41:07 +0300

    final

--- src/lib/userLogIn.js
+++ src/lib/userLogIn.js
@@ -5,7 +5,7 @@ const URL_BASE = process.env.NEXT_PUBLIC_API_URL
 
 
 let userLogIn = async () => {
-    let data = await axios.get(`${URL_BASE}/api/attendance`)
+    let data = await axios.get(`${URL_BASE}/attendance`)
     // let user = data.data.find((user) => user.username === userName && user.password === password)
    return await data.data
 }

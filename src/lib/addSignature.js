3457490b749d861cf9afa939499ba2b2699e135c
mostafa swifi <79422101+mostafaswifi@users.noreply.github.com>
2025-10-27 12:30:32 +0300

    Swap coordinates in addSignature function

--- src/lib/addSignature.js
+++ src/lib/addSignature.js
@@ -8,8 +8,8 @@ let addSignature = async ({employeeId,cordx,cordy}) => {
   // if (!employeeId) return;
     const data = {
       employeeId: employeeId,
-      cordx: cordx,
-      cordy: cordy,
+      cordx: cordy,
+      cordy: cordx,
 
     }
     
